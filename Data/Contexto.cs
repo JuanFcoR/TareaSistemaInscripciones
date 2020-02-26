bf8b51346b2fea69dbc08400e8ef311416b9fec2
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-02-26 03:31:33 -0400

    Creando el contexto y el  Modelo

---
+++ Data/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TareaSistemaInscripciones.Data
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Inscripciones> Inscripciones { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Database/Data.db");
+        }
+    }
+}
---
+++ Models/Inscripciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TareaSistemaInscripciones.Models
+{
+    public class Inscripciones
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        public string Semestre { get; set; }
+        public int Limite { get; set; }
+        public int Tomados { get; set; }
+        public int Disponibles { get; set; }
+
+        public Inscripciones()
+        {
+            InscripcionId = 0;
+            Semestre = String.Empty;
+            Limite = 0;
+            Tomados = 0;
+            Disponibles = 0;
+        }
+    }
+}
